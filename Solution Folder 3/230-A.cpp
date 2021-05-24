a7d3e40e44b976a4cba1b6f31c91bb1fbc6e90a1
Sakshikulkarni880 <64360642+Sakshikulkarni880@users.noreply.github.com>
2021-05-24 23:21:11 +0530

    Update comments

--- Solution Folder 3/230-A.cpp
+++ Solution Folder 3/230-A.cpp
@@ -1,37 +1,3 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//     int s,n;
-//     cin>>s>>n;
-//     int a[n],b[n];
-//     string ans="YES";
-//     for(int i=0;i<n;i++){
-//         cin>>a[i];
-//         cin>>b[i];
-//     }
-//     int temp[n];
-//     for(int i=0;i<n;i++){
-//         temp[i]=a[i];
-//     }
-//     sort(temp,temp+n);
-//     for(int i=0;i<n;i++){
-//         if(s>temp[i]){
-//            int itr = find(a, a + n, temp[i]);
-//             s += b[itr];
-//         }
-
-//         else{
-//             ans="NO";
-//         }
-//     }
-//     cout<<ans<<endl;
-//     return 0;
-// }
-
-
-// // auto itr = find(a, a + n, elem);
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -59,19 +25,13 @@ int main(){
            int index = it - a.begin();
            a[index] = 0;
             s += b[index];
-            // cout<<s<<endl;
-            // cout<<i<<endl;
-            // cout<<a[index]<<endl;
         }
 
         else{
             ans="NO";
-            // cout<<s<<endl;
-            // cout<<i<<endl;
             break;
         }
     }
-    // cout<<s<<endl;
     cout<<ans<<endl;
     return 0;
 }
