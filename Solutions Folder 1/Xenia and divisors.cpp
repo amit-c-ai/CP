4b1d8cd8d8e1b6a819e8ac03044d33a226a59e78
amit-c-ai <amitchoudhary9425@gmail.com>
2021-09-06 19:21:25 +0530

    pending

---
+++ Solutions Folder 1/Xenia and divisors.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+
+int main(void){
+	int n; cin>>n;
+	vector<int> v;
+	for(int i=0; i<n; i++){
+		v.pb(cin>>temp);
+	}
+	v.sort(all(v));
+	
+	return 0;
+}
