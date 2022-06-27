cdcaf48746dc386dfe1e632ef8587e79fd16d703
amit-c-ai <amitchoudhary9425@gmail.com>
2022-06-27 22:29:37 +0530

    rsl questions

---
+++ Interview Questions/RSL/6_nearest_prime.cpp
@@ -0,0 +1,15 @@
+// 6] Write a function that takes an integer as an input and returns the
+// nearest prime number.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int n){
+    
+}
+
+int main(){
+    int n=;
+    solve(n);
+    return 0;
+}
