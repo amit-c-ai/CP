13c000126e6cda8c79792c6574b242c694b76c5c
amit-c-ai <amitchoudhary9425@gmail.com>
2022-06-28 09:41:42 +0530

    rsl interview questions

--- Interview Questions/RSL/6_nearest_prime.cpp
+++ Interview Questions/RSL/6_nearest_prime.cpp
@@ -4,12 +4,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPrime(int n){
+    if(n<=1){
+        return false;
+    }
+    if(n==2 || n==3){
+        return true;
+    }
+    if(n%2==0 || n%3==0){
+        return false;
+    }
+    for(int i=5; i*i<n; i+=6){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int rightPrime(int n){
+    if(n<=1){
+        return -1;
+    }
+    while(!isPrime(n) && n>=2){
+        n++;
+    }
+    return n;
+}
+
+int leftPrime(int n){
+    if(n<=1){
+        return -1;
+    }
+    while(!isPrime(n) && n>=2){
+        n--;
+    }
+    return n;
+}
+
 void solve(int n){
-    
+    int left, right;
+    left = leftPrime(n-1);
+    right = rightPrime(n+1);
+
+    if(n-left > right-n){
+        cout<<right;
+    }
+    else if(right-n > n-left){
+        cout<<left;
+    }
+    else{
+        cout<<left<<" "<<right;
+    }
 }
 
 int main(){
-    int n=;
+    int n=20;
     solve(n);
     return 0;
 }
