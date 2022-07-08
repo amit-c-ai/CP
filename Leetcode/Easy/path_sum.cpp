411da576293188dc628c8f4df82c40c41e4b5799
amit-c-ai <amitchoudhary9425@gmail.com>
2022-07-08 20:12:37 +0530

    path sum

---
+++ Leetcode/Easy/path_sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL){
+            return false;
+        }
+        if(!root->left && !root->right && root->val==targetSum){return true;}
+        
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
