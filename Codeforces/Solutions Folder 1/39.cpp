302a6c87712cfd864ab783211e3b5c04365e8d8b
amit-c-ai <amitchoudhary9425@gmail.com>
2022-07-10 11:48:17 +0530

    numbers changed

--- Codeforces/Solutions Folder 1/39.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(void){
-	int n;
-	cin>>n;
-	cout<<"0 0 "<<n;
-	return 0;
-}
--- Codeforces/Solutions Folder 1/8.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(void){
-	string s;
-	cin>>s;
-	s[0]=toupper(s[0]);
-	cout<<s;
-}
