9381b4421fb48ff3ebbad47a6a9d5d554d7b372f
amit-c-ai <amitchoudhary9425@gmail.com>
2022-07-14 18:26:32 +0530

    n-queens

---
+++ Leetcode/Hard/n_queens.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool isSafe(vector<string> board, int row, int col, int n){
+        int r = row;
+        int c = col;
+        
+        while(row>=0 && col>=0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            row--;
+            col--;
+        }
+        
+        col = c;
+        row = r;
+        while(col>=0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            col--;
+        }
+        
+        col = c;
+        row = r;
+        while(row<n && col>=0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            row++;
+            col--;
+        }
+        
+        return true;
+    }
+    
+public:
+    void solve(vector<string> &board, vector<vector<string>> &ans, int col, int n){
+        if(col>=n){
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row=0; row<n; row++){
+            if(isSafe(board, row, col, n)){
+                board[row][col] = 'Q';
+                solve(board, ans, col+1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+public:
+    vector<vector<string>> solveNQueens(int n) {    
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for(int i=0; i<n; i++){
+            board[i] = s;
+        }
+        solve(board, ans, 0, n);
+        return ans;
+    }
+};
