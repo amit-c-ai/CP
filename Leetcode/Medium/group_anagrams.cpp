0b45ecda34fbb8d97ac8bdeb31ebe7b7c4c3eae1
amit-c-ai <amitchoudhary9425@gmail.com>
2022-07-28 21:54:43 +0530

    group anagrams

---
+++ Leetcode/Medium/group_anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+        string temp;
+        for(auto i: strs){
+            temp = i;
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(i);
+        }
+        for(auto i: mp){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
