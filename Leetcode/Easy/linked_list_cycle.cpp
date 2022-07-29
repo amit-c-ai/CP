426c430cd6fb50e82a6da026b4abdd5b41fb44e4
amit-c-ai <amitchoudhary9425@gmail.com>
2022-07-29 23:23:19 +0530

    linked list cycle

---
+++ Leetcode/Easy/linked_list_cycle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL){
+            return false;
+        }
+        ListNode *fast=head, *slow=head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+};
