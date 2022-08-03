ee4372ee7a85d9d4a031c942bf2f236534d8d66b
amit-c-ai <amitchoudhary9425@gmail.com>
2022-08-03 21:10:04 +0530

    my calendar 1

---
+++ Leetcode/Medium/my_calendar_1.cpp
@@ -0,0 +1,14 @@
+class MyCalendar {
+    map<int, int> mp;
+public:  
+    bool book(int start, int end) {
+        auto itr = mp.upper_bound(start);
+        if(itr == mp.end() || end<= itr->second) {
+            mp[end]=start;
+            return true;
+        } 
+        else{
+            return false;
+        }
+    }
+};
